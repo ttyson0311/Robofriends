da292005325b5f379c20b785da990c4910fdf93a
ttyson0311 <ttyson0311@gmail.com>
2019-02-27 19:17:45 -0500

    Updates

---
+++ precache-manifest.65df2be7dfd3c4c849e7e40e9179a665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ttyson0311/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "683244f7398db88bd6fd",
+    "url": "/ttyson0311/robofriends.git/static/js/runtime~main.683244f7.js"
+  },
+  {
+    "revision": "6c49027c9dc3c458cd93",
+    "url": "/ttyson0311/robofriends.git/static/js/main.6c49027c.chunk.js"
+  },
+  {
+    "revision": "c66a3ff0937c28a7cc34",
+    "url": "/ttyson0311/robofriends.git/static/js/2.c66a3ff0.chunk.js"
+  },
+  {
+    "revision": "6c49027c9dc3c458cd93",
+    "url": "/ttyson0311/robofriends.git/static/css/main.213a88d0.chunk.css"
+  },
+  {
+    "revision": "c66a3ff0937c28a7cc34",
+    "url": "/ttyson0311/robofriends.git/static/css/2.32dfd30d.chunk.css"
+  },
+  {
+    "revision": "a10b455ccab1806b2621ef5bd057a03c",
+    "url": "/ttyson0311/robofriends.git/index.html"
+  }
+];
